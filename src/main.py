47394b3a4dea8fae6bddd15f8b92550bc7a49426
arne <arne@decsai.ugr.es>
2021-02-02 19:37:48 +0100

    clean up main.py

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from vgpmil.helperfunctions import RBF
 from vgpmil.vgpmil import vgpmil
 from typing import Dict
 
-from loading import convert_to_vgpmil_input
+from loading import convert_to_vgpmil_input, load_cnn_predictions
 from metrics import calc_instance_level_metrics, calc_bag_level_metrics
 
 
@@ -23,38 +23,29 @@ def test(vgpmil_model: vgpmil, config: Dict):
     test_df = pd.read_csv(config['path_test_df'])
     print('Loaded test dataframe. Number of instances: ' + str(len(test_df)))
     features, bag_labels_per_instance, bag_names_per_instance, _, _, _, instance_labels = convert_to_vgpmil_input(test_df, config)
+
+    # calculate the metrics for vgpmil
     predictions = vgpmil_model.predict(features)
-    bag_metrics = calc_bag_level_metrics(predictions, bag_labels_per_instance, bag_names_per_instance, 'vgpmil')
-
     predictions = np.where(predictions >= 0.5, 1, 0).astype("float32")
-    if instance_labels is not None:
-        instance_metrics = calc_instance_level_metrics(predictions, instance_labels, 'vgpmil')
-        metrics = pd.concat([bag_metrics, instance_metrics], axis=0)
-    else:
-        metrics = bag_metrics
+    vgpmil_instance_metrics = calc_instance_level_metrics(predictions, instance_labels, 'vgpmil')
+    vgpmil_bag_metrics = calc_bag_level_metrics(predictions, bag_labels_per_instance, bag_names_per_instance, 'vgpmil')
+    vgpmil_metrics = pd.concat([vgpmil_instance_metrics, vgpmil_bag_metrics], axis=0)
+
+    # calculate the metrics for the CNN
+    cnn_predictions, bag_cnn_predictions = load_cnn_predictions(test_df, config)
+    cnn_instance_metrics = calc_instance_level_metrics(cnn_predictions, instance_labels, 'cnn')
+    cnn_bag_metrics = calc_bag_level_metrics(bag_cnn_predictions, bag_labels_per_instance, bag_names_per_instance, 'cnn')
+
+    cnn_metrics = pd.concat([cnn_instance_metrics, cnn_bag_metrics], axis=0)
+
+    metrics = pd.concat([vgpmil_metrics, cnn_metrics], axis=1)
+
     out_name = os.path.basename(config['path_test_df']).split('.')[0]
-
-    col_cnn_prediction = config['col_cnn_prediction']
-    if col_cnn_prediction in test_df.columns:
-        print('Found CNN predictions. Comparing..')
-        cnn_predictions = test_df[col_cnn_prediction].to_numpy().astype("float32")
-        cnn_instance_metrics = calc_instance_level_metrics(cnn_predictions, instance_labels, 'cnn')
-
-        col_bag_cnn_predictions = config['col_bag_cnn_predictions']
-        bag_cnn_predictions = None
-        if col_bag_cnn_predictions in test_df.columns:
-            print('Using CNN bag predictions')
-            bag_cnn_predictions = (test_df[col_bag_cnn_predictions].to_numpy().astype("int"))
-        else: # derive bag predictions from instance predictions if not available
-            bag_cnn_predictions = cnn_predictions
-        cnn_bag_metrics = calc_bag_level_metrics(bag_cnn_predictions, bag_labels_per_instance,
-                                                      bag_names_per_instance, 'cnn')
-        cnn_metrics = pd.concat([cnn_instance_metrics, cnn_bag_metrics], axis=0)
-        metrics = pd.concat([metrics, cnn_metrics], axis=1)
-
     out_file = os.path.join(config['output_path'], 'metrics_' + out_name + '.csv')
+
     print('Save output to ' + out_file)
     metrics.to_csv(out_file)
+
 
 
 def main():
