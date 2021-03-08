e244a3976adf9cdde1798421dfeb7699daa1ef44
arne <arne@decsai.ugr.es>
2021-03-08 13:22:53 +0100

    tsne visualization

--- src/tsne_visualization.py
+++ src/tsne_visualization.py
@@ -16,12 +16,14 @@ def visualize_and_save(features, instance_labels, config):
     plt.scatter(features_embedded_id_0[:,0], features_embedded_id_0[:,1], c="g", alpha=0.5, marker='o',
                 label="normal")
     plt.scatter(features_embedded_id_1[:,0], features_embedded_id_1[:,1], c="r", alpha=0.5, marker='+',
-                label="ITC")
-    plt.legend(loc='upper left')
+                label="ICH")
+    #plt.legend(loc='upper left')
+    fig = plt.gcf()
+    fig.set_size_inches(4, 4)
     # plt.show()
     print('Save TSNE plot to: ')
     out_name = os.path.basename(config['path_test_df']).split('.')[0]
     out_file = os.path.join(config['output_path'], 'tsne_' + out_name + '.jpg')
     os.makedirs(config['output_path'], exist_ok=True)
     print('Save TSNE plot to: ' + out_file)
-    plt.savefig(out_file)
+    fig.savefig(out_file)
