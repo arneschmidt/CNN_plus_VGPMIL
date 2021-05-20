e6f505d1e9888b0e4d5422883b1ed523df75ddec
arne <arne@decsai.ugr.es>
2021-05-20 13:30:47 +0200

    update configs

--- src/run_rsna.sh
+++ src/run_rsna.sh
@@ -1,4 +1,4 @@
-#python ./src/main.py -c ./configs/config_rsna_0.yaml
+python ./src/main.py -c ./configs/config_rsna_0.yaml
 python ./src/main.py -c ./configs/config_rsna_1.yaml
 python ./src/main.py -c ./configs/config_rsna_2.yaml
 python ./src/main.py -c ./configs/config_rsna_3.yaml
