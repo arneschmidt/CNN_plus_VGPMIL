039935800872bb0cc704a811f59e297fd6d4c7a5
arne <arne@decsai.ugr.es>
2021-07-01 12:50:58 +0200

    add corrected tables and update configs

---
+++ run_files/run_rsna_aw.sh
@@ -0,0 +1,5 @@
+python ./src/main.py -c ./configs/config_rsna_aw_0.yaml
+python ./src/main.py -c ./configs/config_rsna_aw_1.yaml
+python ./src/main.py -c ./configs/config_rsna_aw_2.yaml
+python ./src/main.py -c ./configs/config_rsna_aw_3.yaml
+python ./src/main.py -c ./configs/config_rsna_aw_4.yaml
